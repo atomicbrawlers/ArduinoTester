9d57a3928b68a5d5970a10ccd3c6c0885a3959d4
Matt Jordan <atomicbrawlers@gmail.com>
2018-02-25 15:20:04 -0500

    works

--- app/src/main/java/matt/arduinotester/Blink.java
+++ app/src/main/java/matt/arduinotester/Blink.java
@@ -1,23 +1,146 @@
 package matt.arduinotester;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
 
 public class Blink extends AppCompatActivity {
+
+    private BluetoothAdapter BA;
+    private Set<BluetoothDevice> pairedDevices;
+    ListView lv;
+    BluetoothDevice mDevice;
+    private BluetoothSocket mSocket;
+    OutputStream mOutStream;
+    Boolean temp = false;
+    private UUID myUUID;
+    private final String UUID_STRING_WELL_KNOWN_SPP =
+            "00001101-0000-1000-8000-00805F9B34FB";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blink);
+
+        myUUID = UUID.fromString(UUID_STRING_WELL_KNOWN_SPP);
+
+        BA = BluetoothAdapter.getDefaultAdapter();
+
+        lv = findViewById(R.id.listView);
+        gatherAndPrintList();
+
+        mDevice = null;
+
+        pairedDevices = BA.getBondedDevices();
+        for (BluetoothDevice bt : pairedDevices) {
+            System.out.println(bt.getName());
+            if (bt.getName().equals("matt HC-05")) { //TODO: CHANGE TO YOUR DEVICE NAME
+                System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n");
+                mDevice = bt;
+            }
+        }
+
+        try {
+            mSocket = mDevice.createRfcommSocketToServiceRecord(myUUID);
+        } catch (IOException e) {
+            mSocket = null;
+        }
+
+        Boolean tmp = true;
+
+        try {
+            mSocket.connect();
+        } catch (IOException connectException) {
+            try {
+                mSocket.close();
+                tmp = false;
+            } catch (IOException closeException) {
+            }
+        }
+
+        mOutStream = null;
+        if (tmp) {
+            try {
+                mOutStream = mSocket.getOutputStream();
+            } catch (IOException e) {
+            }
+        }
     }
 
-    public void button1Pressed(View view){
+    //Gathers and prints list of bluetooth devices (NOT NEEDED) ------------------------->
+    public void gatherAndPrintList(){
+        on(lv);
+        visible(lv);
+        list(lv);
+    }
+
+    //Methods used for gatherAndPrintList
+    public void on(View v){
+        if (!BA.isEnabled()) {
+            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnOn, 0);
+            Toast.makeText(getApplicationContext(), "Turned on",Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Already on", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void off(View v){
+        BA.disable();
+        Toast.makeText(getApplicationContext(), "Turned off" ,Toast.LENGTH_LONG).show();
+    }
+
+
+    public  void visible(View v){
+        Intent getVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        startActivityForResult(getVisible, 0);
+    }
+
+
+    public void list(View v){
+        pairedDevices = BA.getBondedDevices();
+
+        ArrayList list = new ArrayList();
+
+        for(BluetoothDevice bt : pairedDevices) list.add(bt.getName());
+        Toast.makeText(getApplicationContext(), "Showing Paired Devices",Toast.LENGTH_SHORT).show();
+
+        final ArrayAdapter adapter = new  ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+
+        lv.setAdapter(adapter);
+    } // <-----------------------------------------------------------------------------------------
+
+    public void button1Pressed(View view){ //Currently does nothing
         System.out.println("Button 1 Pressed");
     }
 
     public void button2Pressed(View view){
         System.out.println("Button 2 Pressed");
+
+        try {
+            if (temp)
+                mOutStream.write("0".getBytes());
+            else
+                mOutStream.write("1".getBytes());
+        } catch (IOException e) {
+
+        }
+        temp = !temp;
+
     }
 
 }
